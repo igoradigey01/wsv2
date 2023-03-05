598bce2e5d3182fceaf4af49652de117955e40a3
igoradigey01 <ett66@mail.ru>
2023-03-05 21:29:56 +0300

    fix(app.route*path)

--- apps/shagen01/src/app/app.routes.ts
+++ apps/shagen01/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+    {
+        path: 'account',              
+        loadChildren: () =>
+          import('@wsv2/account').then((m) => m.AccountModule),
+      }
+];
+
