86ef0a5e1d7f385943774eb6e53693ba476d701a
igoradigey01 <ett66@mail.ru>
2023-10-11 21:01:53 +0300

    chore(log-in)

--- libs/account/src/lib/sign-in/sign-in.component.ts
+++ libs/account/src/lib/sign-in/sign-in.component.ts
@@ -12,21 +12,20 @@ import {
 } from '@abacritt/angularx-social-login';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import {  NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { UserManagerService } from '@wsv2/account-service';
 import { AccountService } from '../_shared/services/account.service';
 import { Subscription } from 'rxjs';
 import { ExternalAuthSocialDto } from '../_shared/_interfaces/ExternalAuthSocialDto.model';
 
-import {VkLoginWidgetService} from '../_shared/services/vk-login-widger.service'
-
+import { VkLoginWidgetService } from '../_shared/services/vk-login-widger.service';
 
 //https://code-maze.com/angular-security-with-asp-net-core-identity/
 //https://account.mail.ru/user/2-step-auth/passwords/
 //https://help.mail.ru/mail/security/protection/external
 //https://docs.microsoft.com/ru-ru/aspnet/core/security/authentication/accconfirm?view=aspnetcore-6.0&tabs=netcore-cli
 // external login goole https://www.positronx.io/angular-google-social-login-tutorial-with-example/
-// 
+//
 //https://github.com/abacritt/angularx-social-login
 
 @Component({
@@ -46,17 +45,18 @@ export class SignInComponent implements OnInit, OnDestroy {
   _isUserInvalid = false;
 
   // parser file on load
+  public login = '';
   public password = '';
-  public email = '';
-  public phone='';
+ // private email = '';
+  //private phone = '';
   public rememberme = true;
   public returnUrl = '/';
   //http://jsonip.com/
   public ipAddress = '';
-  public nameClient:string;
+  public nameClient: string;
 
-  public get ClintGoogleUrl(){
-    return this.repozitory.ClientUri+'account/auth-callback-vk'
+  public get ClintGoogleUrl() {
+    return this.repozitory.ClientUri + 'account/auth-callback-vk';
   }
 
   constructor(
@@ -64,109 +64,114 @@ export class SignInComponent implements OnInit, OnDestroy {
     private userManager: UserManagerService,
     private route: ActivatedRoute,
     private socialAuthService: SocialAuthService,
-    private repozitoryVK:VkLoginWidgetService,
+    private repozitoryVK: VkLoginWidgetService,
     private router: Router
   ) {
-    this.nameClient=repozitory.ClientId + "" +repozitory.VkId;
-    console.log("VkId"+repozitory.VkId)
-    
+    this.nameClient = repozitory.ClientId;
+    // console.log("VkId"+repozitory.VkId)
   }
 
   ngOnInit(): void {
-    
-   
-
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    const subGoogle =   this.socialAuthService.authState.subscribe((user) => {
+    const subGoogle = this.socialAuthService.authState.subscribe((user) => {
       // this.socialUser = user;
-      const credentialsGoogle=<ExternalAuthSocialDto>{provider:user.provider,idToken:user.idToken,idUser:user.id};
-    
-    if(user.provider=="GOOGLE"){
-      
-      const subApiGoogle=    this.repozitory.googleLogin(credentialsGoogle).subscribe({
-        next: (d) => {
-          this.userManager.setInvalidLogin$(false, d.access_token);
-          //  console.log("login_in-"+d.access_token)
-          this.router.navigate([this.returnUrl]);
-         
-        },
-        error: (err: HttpErrorResponse) => {
-          let body='';
-          this.userManager.setInvalidLogin$(true, null);
-         
-          if (err.status === 401) {
-            this._errorMgs.push('пользователь не авторизован,войдите на сайт');
-            this._errorMgs.push(err.error);
-            return;
-          }
-          if (err.status == 400) {
-            this._errorMgs.push(' 400 Bad Request');
-            this._errorMgs.push(err.error);
-  
-            return;
-  
-            //  body = 'Не верный логин или пароль';
-          }
-  
-          body =
-            'Ошибка соединения с сервером -Сообщиете Администаратору Pесурса';
-  
-          this._errorMgs.push(body);
-        },
-      });
-     this._subscriptions.push(subApiGoogle);
-    }
-    if(user.provider=="VK"){
-      // https://vk.com/dev/widget_auth (!!!)
-      //https://dev.vk.com/api/open-api/getting-started
-      const  credentialsVK=<ExternalAuthSocialDto>{provider:user.provider,idToken:user.authToken,idUser:user.id}
-     // credentials.idToken=user.idToken ;
-      // credentials.idToken=user.authToken
-      // credentials.provider="VK";
-      // credentials.idUser=user.id;
+      const credentialsGoogle = <ExternalAuthSocialDto>{
+        provider: user.provider,
+        idToken: user.idToken,
+        idUser: user.id,
+      };
 
-      const subApiVK=    this.repozitory.vkLogin(credentialsVK).subscribe({
+      if (user.provider == 'GOOGLE') {
+        const subApiGoogle = this.repozitory
+          .googleLogin(credentialsGoogle)
+          .subscribe({
             next: (d) => {
-              
               this.userManager.setInvalidLogin$(false, d.access_token);
-              //  console.log("login_in-"+d.access_token)
+               // console.log("login_in-"+d.access_token)
               this.router.navigate([this.returnUrl]);
-             
             },
             error: (err: HttpErrorResponse) => {
-              let body="";
+              let body = '';
               this.userManager.setInvalidLogin$(true, null);
-             
+
               if (err.status === 401) {
-                this._errorMgs.push('пользователь не авторизован,войдите на сайт');
+                this._errorMgs.push(
+                  'пользователь не авторизован,войдите на сайт'
+                );
                 this._errorMgs.push(err.error);
                 return;
               }
               if (err.status == 400) {
                 this._errorMgs.push(' 400 Bad Request');
                 this._errorMgs.push(err.error);
-      
+
                 return;
-      
+
                 //  body = 'Не верный логин или пароль';
               }
-      
+
               body =
                 'Ошибка соединения с сервером -Сообщиете Администаратору Pесурса';
-      
+
               this._errorMgs.push(body);
             },
           });
-         this._subscriptions.push(subApiVK);
+        this._subscriptions.push(subApiGoogle);
+      }
+      if (user.provider == 'VK') {
+        // https://vk.com/dev/widget_auth (!!!)
+        //https://dev.vk.com/api/open-api/getting-started
+        const credentialsVK = <ExternalAuthSocialDto>{
+          provider: user.provider,
+          idToken: user.authToken,
+          idUser: user.id,
+        };
+        // credentials.idToken=user.idToken ;
+        // credentials.idToken=user.authToken
+        // credentials.provider="VK";
+        // credentials.idUser=user.id;
 
-        }
-  //  console.log(user)
+        const subApiVK = this.repozitory.vkLogin(credentialsVK).subscribe({
+          next: (d) => {
+            this.userManager.setInvalidLogin$(false, d.access_token);
+            //  console.log("login_in-"+d.access_token)
+            this.router.navigate([this.returnUrl]);
+          },
+          error: (err: HttpErrorResponse) => {
+            let body = '';
+            this.userManager.setInvalidLogin$(true, null);
+
+            if (err.status === 401) {
+              this._errorMgs.push(
+                'пользователь не авторизован,войдите на сайт'
+              );
+              this._errorMgs.push(err.error);
+              return;
+            }
+            if (err.status == 400) {
+              this._errorMgs.push(' 400 Bad Request');
+              this._errorMgs.push(err.error);
+
+              return;
+
+              //  body = 'Не верный логин или пароль';
+            }
+
+            body =
+              'Ошибка соединения с сервером -Сообщиете Администаратору Pесурса';
+
+            this._errorMgs.push(body);
+          },
+        });
+        this._subscriptions.push(subApiVK);
+      }
+      //  console.log(user)
     });
 
-   const subLogin = this.userManager.InvalidLogin$.subscribe((d) => {
+    const subLogin = this.userManager.InvalidLogin$.subscribe((d) => {
       this._isUserInvalid = d;
       if (!d) {
-        this.router.navigate([this.returnUrl]);;
+        this.router.navigate([this.returnUrl]);
       }
     });
     this._subscriptions.push(subGoogle);
@@ -177,36 +182,50 @@ export class SignInComponent implements OnInit, OnDestroy {
     this._subscriptions.forEach((s) => s.unsubscribe());
   }
 
-
- 
   signInWithVK(): void {
-
-    // три способа афторизации 
+    // три способа афторизации
     //socialAuthServic.signIn(VKLoginProvider.PROVIDER_ID) https://api.vk.com/method/users.get
     //https://dev.vk.com/widgets/auth
     //https://dev.vk.com/vk-sdk/vkid/auth/main#По%20кнопке%20One%20Tap%20Sign%20In
     //debugger
-   // this.socialAuthService.signIn(VKLoginProvider.PROVIDER_ID);
-   this.repozitoryVK.loadWidgetScript();
+    // this.socialAuthService.signIn(VKLoginProvider.PROVIDER_ID);
+    this.repozitoryVK.loadWidgetScript();
   }
 
-
-
-
-  submitForm(loginForm: NgForm) {
+  submitForm() {
     this._errorMgs = [];
 
-    const credentials = JSON.stringify(loginForm.value);
+    let credentials: string; //= JSON.stringify(loginForm.value);
+
+    if (this.login.indexOf('@') != -1) {
+      credentials = `{
+        "email":"${this.login}",
+        "phone": "",
+        "password":"${this.password}",
+        "spaId":"${this.repozitory.ClientId}",
+        "remembermy":"${this.rememberme}"
+      }`;
+    }
+    else{
+
+      credentials = `{
+        "email":"",
+        "phone": "${this.login}",
+        "password":"${this.password}",
+        "spaId":"${this.repozitory.ClientId}",
+        "remembermy":"${this.rememberme}"
+      }`;
+
+    }
 
     const subLogin2 = this.repozitory.login(credentials).subscribe({
       next: (d) => {
         this.userManager.setInvalidLogin$(false, d.access_token);
-        //  console.log("login_in-"+d.access_token)
+        console.log("login_in-"+d.access_token)
         this.router.navigate([this.returnUrl]);
-       
       },
       error: (err: HttpErrorResponse) => {
-        let body="";
+        let body = '';
         this.userManager.setInvalidLogin$(true, null);
         console.error(err);
         if (err.status === 401) {
@@ -234,16 +253,16 @@ export class SignInComponent implements OnInit, OnDestroy {
   }
 
   onFileInput(event: any) {
-   const data = event.target.files[0];
+    const data = event.target.files[0];
 
     const fr = new FileReader();
     fr.readAsText(data);
     fr.onload = () => {
-      console.log('Input-file cheng ok------' + fr.result);
+      //console.log('Input-file cheng ok------' + fr.result);
       const coolVar = fr.result as string;
 
-    const partsArray = coolVar.split(';');
-      this.email = partsArray[0].trim();
+      const partsArray = coolVar.split(';');
+      this.login = partsArray[0].trim();
       this.password = partsArray[1].trim();
 
       /*  console.log(this._log+"----log----");
@@ -255,20 +274,19 @@ export class SignInComponent implements OnInit, OnDestroy {
   }
 
   private validateExternalAuth(externalAuth: ExternalAuthSocialDto) {
-    this.repozitory.googleLogin( externalAuth)
-      .subscribe({
-        next: (d:any) => {
-           // localStorage.setItem("token", d.token);
-            this.userManager.setInvalidLogin$(false, d.access_token);
+    this.repozitory.googleLogin(externalAuth).subscribe({
+      next: (d: any) => {
+        // localStorage.setItem("token", d.token);
+        this.userManager.setInvalidLogin$(false, d.access_token);
 
-           // this.repozitory.sendAuthStateChangeNotification(d.isAuthSuccessful);
-            this.router.navigate([this.returnUrl]);
+        // this.repozitory.sendAuthStateChangeNotification(d.isAuthSuccessful);
+        this.router.navigate([this.returnUrl]);
       },
-        error: (err: HttpErrorResponse) => {
-          this._errorMgs.push( err.message);
-         
-          this.socialAuthService.signOut();
-        }
-      });
+      error: (err: HttpErrorResponse) => {
+        this._errorMgs.push(err.message);
+
+        this.socialAuthService.signOut();
+      },
+    });
   }
 }
