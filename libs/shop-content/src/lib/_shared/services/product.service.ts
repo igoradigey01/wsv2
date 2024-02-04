98788e900daa18f2cabb6a61fd78f216311962d0
igoradigey01 <ett66@mail.ru>
2024-02-04 09:52:02 +0300

    chore(SubCatalogProduct servise load)

--- libs/shop-content/src/lib/_shared/services/product.service.ts
+++ libs/shop-content/src/lib/_shared/services/product.service.ts
@@ -1,25 +1,25 @@
-import { Injectable, signal,computed } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
 
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { ApiService } from '@wsv2/app-config';
-import { Product, Article, Brand, Color, Status ,Message} from '@wsv2/app-common';
+import { Product, Status, Message } from '@wsv2/app-common';
+//import {Brand, Color,} from '@wsv2/shop-content'
 import { SubKatalogService } from './sub-katalog.service';
-import {ArticleService} from './article.service'
-import {BrandService} from './brand.service'
-import {ColorService} from './color.service'
-import {ProductTypeService} from './product_type.service'
+import { ArticleService } from './article.service';
+import { BrandService } from './brand.service';
+import { ColorService } from './color.service';
+import { ProductTypeService } from './product_type.service';
 
 import { catchError, of, tap, map } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-
 interface ProductListState {
   productItems: Product[];
 
-  queryAdd:  Product[];
-  queryUpdate:  Product[];
-  queryDelete:  Product[];
+  queryAdd: Product[];
+  queryUpdate: Product[];
+  queryDelete: Product[];
 
   state: Status;
 }
@@ -28,9 +28,6 @@ interface ProductListState {
   providedIn: 'root',
 })
 export class ProductService {
-
-
-
   private state = signal<ProductListState>({
     productItems: [],
     queryAdd: [],
@@ -41,19 +38,14 @@ export class ProductService {
 
   private error_state = signal<Message>({ message: undefined, error: false });
 
-  readonly Articles = this.articleRepository.Articles
-  readonly Brands = this.brandRepository.Brands
-  readonly Colors = this.colorRepository.Colors
+  readonly Articles = this.articleRepository.Articles;
+  readonly Brands = this.brandRepository.Brands;
+  readonly Colors = this.colorRepository.Colors;
 
-  readonly TypeProducts =this.product_typeRepository.ProductTypes;
- 
+  readonly TypeProducts = this.product_typeRepository.ProductTypes;
 
- 
-
-  readonly   Products = computed(() => this.state().productItems);
+  readonly Products = computed(() => this.state().productItems);
   readonly Message = computed(() => this.error_state());
-
-
 
   ProductItem = signal<Product>({
     id: 0,
@@ -85,65 +77,39 @@ export class ProductService {
   });
 
   private LoadSubCatalogProduct(idSubCatlog: number) {
-
-    this.SubCatalogProduct$(idSubCatlog)
+    this.SubCatalogProduct$(idSubCatlog);
   }
 
   public ReLoadSubCatalogProduct(idSubCatlog: number) {
-    this.SubCatalogProduct$(idSubCatlog)
+    this.SubCatalogProduct$(idSubCatlog);
   }
 
   constructor(
     private _http: HttpClient,
-    private _url: ApiService,
+    private url: ApiService,
     private subKatalogRepository: SubKatalogService,
-    private  articleRepository:ArticleService,
-    private  colorRepository :ColorService,
-    private  brandRepository:BrandService,
-    private  product_typeRepository:ProductTypeService
-  ) {
-    
-  }
+    private articleRepository: ArticleService,
+    private colorRepository: ColorService,
+    private brandRepository: BrandService,
+    private product_typeRepository: ProductTypeService
+  ) {}
 
   private SubCatalogProduct$ = (idSubKatlog: number): void => {
-    this.url.Controller = 'Article';
-    this.url.Action = 'GetAll';
+    this.url.Controller = 'Product';
+    this.url.Action = 'GetForSubCatalog';
     this.url.ID = this.url.ClientId;
-    this._http
-      .get<Article[]>(this.url.Url, { headers: this.headers })
-      .pipe(
-        tap((data) => {
-          //  this.state.update(() => data);
-          this.state.update((state) => ({
-            ...state,
-            productItems: data,
-            state: Status.load,
-          }));
-        }),
-        //https://angularindepth.com/posts/1518/takeuntildestroy-in-angular-v16
-        takeUntilDestroyed(),
-        catchError(() => of([] as Article[])) //  on any error, just return an empty array
-      )
-      .subscribe();
-
-    ///------------------------------------
-    this._url.Controller = 'Nomenclature';
-    this._url.Action = 'NomenclaturePKs';
-    this._url.ID = idSubKatlog;
-    const postavchikId = 1; //this.url.PostavchikId;
-
     const headers: HttpHeaders = new HttpHeaders({
       Accept: 'application/json',
       //  Authorization: 'Bearer ' + token,
     });
     const params: HttpParams = new HttpParams().set(
-      'postavchikId',
-      postavchikId
+      'idSubCatalog',
+      idSubKatlog
     );
 
     const httpOptions = { headers, params };
     this._http
-      .get<Product[]>(this._url.Url, httpOptions)
+      .get<Product[]>(this.url.Url, httpOptions)
       .pipe(
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         map((data: any) => {
@@ -170,20 +136,20 @@ export class ProductService {
 
               colorId: f.colorId,
               colorName:
-                this.Color().length > 0
-                  ? this.Color().find((d) => d.id === f.colorId)?.name
+                this.Colors().length > 0
+                  ? this.Colors().find((d) => d.id === f.colorId)?.name
                   : undefined,
 
               brandId: f.brandId,
               brandName:
-                this.Brand().length > 0
-                  ? this.Brand().find((d) => d.id === f.brandId)?.name
+                this.Brands().length > 0
+                  ? this.Brands().find((d) => d.id === f.brandId)?.name
                   : undefined,
 
               articleId: f.articleId,
               articleName:
-                this.Article().length > 0
-                  ? this.Article().find((d) => d.id === f.articleId)?.name
+                this.Articles().length > 0
+                  ? this.Articles().find((d) => d.id === f.articleId)?.name
                   : undefined,
 
               hidden: f.hidden,
@@ -192,126 +158,26 @@ export class ProductService {
             });
           });
         }),
-        tap((data) => this.Products.set(data)),
+        tap((data) => {
+          //  this.state.update(() => data);
+          this.state.update((state) => ({
+            ...state,
+            productItems: data,
+            state: Status.load,
+          }));
+        }),
+        //https://angularindepth.com/posts/1518/takeuntildestroy-in-angular-v16
         takeUntilDestroyed(),
         catchError(() => of([] as Product[])) //  on any error, just return an empty array
       )
       .subscribe();
-  };
 
-  PoductsSet = (idSubKatlog: string): void => {
-    this.Poducts$(+idSubKatlog);
-  };
+    ///------------------------------------
+  }; 
 
-  private ProductItem$ = (idProductItem: number): void => {
-    //debugger
-    this._url.Controller = 'Nomenclature';
-    this._url.Action = 'Item';
-    this._url.ID = idProductItem;
 
-    const headers: HttpHeaders = new HttpHeaders({
-      Accept: 'application/json',
-      //  Authorization: 'Bearer ' + token,
-    });
-    // let params:HttpParams=new HttpParams().set('postavchikId',this._url.PostavchikId)
 
-    const httpOptions = { headers };
-    this._http
-      .get<Product>(this._url.Url, httpOptions)
-      .pipe(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        map((f: any) => {
-          //  console.log(JSON.stringify(data))
-          return <Product>{
-            id: f.id,
-            guid: f.guid,
-            img_guids: f.img_guids ? [...f.img_guids.push(f.guid)] : [f.guid],
-            hidden: f.hidden,
-            ownerId: f.ownerId,
-            product_typeId: f.product_typeId,
-            title: f.title,
-
-            subCatalogId: f.katalogId,
-            subCatalogName: this.subKatalogRepository
-              .SubCatalogs()
-              .find((d) => d.id === f.katalogId)?.name,
-
-            colorId: f.colorId,
-            colorName:
-              this.Color().length > 0
-                ? this.Color().find((d) => d.id === f.colorId)?.name
-                : undefined,
-            // colorName:this.sharedVar.ColorNs.length>0?this.sharedVar.ColorNs.find(d=>d.id===f.articleId)?.name:undefined,
-            brandId: f.brandId,
-            brandName:
-              this.Brand().length > 0
-                ? this.Brand().find((d) => d.id === f.brandId)?.name
-                : undefined,
-
-            articleId: f.articleId,
-            articleName:
-              this.Article().length > 0
-                ? this.Article().find((d) => d.id === f.articleId)?.name
-                : undefined,
-
-            position: f.position,
-
-            inStock: f.inStock,
-            sale: f.sale,
-
-            price: f.price,
-            markup: f.markup,
-
-            cost_total: f.price * (f.markup / 100) + f.price,
-
-            description: f.description,
-            descriptionSeo: f.descriptionSeo,
-          };
-        }),
-        tap((data) => {
-          this.ProductItem.set(data);
-          console.log(JSON.stringify(data));
-        }),
-        takeUntilDestroyed(),
-        catchError(() =>
-          of({
-            id: 0,
-            guid: undefined,
-            img_guids: undefined,
-            hidden: false,
-            ownerId: '',
-            product_typeId: 0,
-            title: '',
-            subCatalogId: 0,
-            subCatalogName: undefined,
-
-            colorId: 0,
-            colorName: undefined,
-            brandId: 0,
-            brandName: undefined,
-            articleId: 0,
-            articleName: undefined,
-
-            position: 0,
-            inStock: false,
-            sale: false,
-            price: 0,
-            markup: 0,
-            cost_total: undefined,
-            description: undefined,
-
-            descriptionSeo: undefined,
-          } as Product)
-        ) //  on any error, just return an empty ProductItem
-      )
-      .subscribe();
-  };
-
-  PoductsItemSet = (idProductItem: string): void => {
-    this.ProductItem$(+idProductItem);
-  };
-
-  ///  for: nomenclatureItem  ___________________
+ 
 
  
 }
